5679ab2a75f6322a0adb1a282e8b6fbf37b57eec
phamthuy1320 <phamthithuy1320@gmail.com>
2020-10-03 00:32:06 +0700

    [BTLT2] update

--- BTLT2/prefixcodetree.py
+++ BTLT2/prefixcodetree.py
@@ -11,59 +11,47 @@ class PrefixCodeTree():
 #  inside each def have to 'self' params
     def __init__(self): 
         self.codebook = [] 
-        self.message = []
-          
+# Chèn symbol và từ mã symbol
     def insert(self, codeword, symbol): 
         self.codebook.append({symbol: codeword})
-
+# Lấy chiều dài của từ mã
     def getLen(self, symbol):
         for sym in self.codebook:
             for i in sym:
                 if i==symbol:
                     return len(sym[i])
-        
+# Tìm trong code xem có từ mã nào ở đầu code không   
     def search(self, code):
-        if len(code)>=0:
-            for symbol in self.codebook:
-                for i in symbol:
-                    length = len(symbol[i])
-                    firstPartCode = []
-                    for idx in range(length):
-                        firstPartCode.append(int(code[idx]))
-                    if symbol[i] ==firstPartCode:
-                        return i
-        else:
-            return 0
+        for symbol in self.codebook:
+            for i in symbol:
+                length = len(symbol[i])
+                firstPartCode = []
+                for idx in range(length):
+                    firstPartCode.append(int(code[idx]))
+                if symbol[i] ==firstPartCode:
+                    return str(i)
 
-
+# Giải mã
     def decode(self,encodedData, datalen):
         hexString = binascii.hexlify(encodedData)
         h2b = bin(int(hexString, 16))
         binString = str(h2b).split('b',1)[1]
-        message = []
+        
         x = slice(0,datalen)
-        binString = binString[x]
-
-        m=self.search(binString)
-        lm1 = 0
-        lm2 = 0
-
-        for i in range(0,4):
-            lm1 = len(message)
-            print('lm1', lm1)
-            message.append(m)
-            l=self.getLen(m)
-            x=slice(l,datalen-l)
-            binString = binString[x]
-            m=self.search(binString)
-            print('m',m)
-            lm2 = len(message)
-            print('lm2', lm2)
-
-        return message
+        m=self.search(binString[x])
+        message=m
+        print('bin:', binString[x], datalen)
+        l=self.getLen(m)
+        while l<datalen:
+            _x=slice(l,datalen)
+            _m=self.search(binString[_x])
+            l=l+self.getLen(_m)
+            message=message+_m
+        else:
+            print('message', message)
+        
 
 test = PrefixCodeTree()
 for symbol in codebook:
     test.insert(codebook[symbol], symbol)
-
-print(test.decode(b'\xd2\x9f\x20', 21))
+test.decode(b'\xd2\x9f\x20', 21)
